800d2e80d08498ec44a5fd2c1ad66469a6918169
Paula Norris <thepandalion@gmail.com>
2020-06-10 07:55:30 +0100

    Add axios and vue-axios for accessing APIs

--- api-example/src/router/index.js
+++ api-example/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import Home from '../views/Home.vue'
+
+Vue.use(VueAxios, axios)
 
 Vue.use(VueRouter)
 
